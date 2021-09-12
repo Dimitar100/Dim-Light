382a318b926090aca01cca55b2b2ee17e99a7ce9
BuildTools <stoilovdimitar29@gmail.com>
2021-09-12 22:49:41 +0300

    The knight can atack right

--- player.gd
+++ player.gd
@@ -1,10 +1,10 @@
 extends KinematicBody2D
 
 const UP = Vector2(0, -1)
-const SPEED = 400
+const SPEED = 500
 const ATACK_SPEED = 200
 const GRAVITY = 2000
-const JUMP = -500
+const JUMP = -350
 const WIND_TIMER = 3
 
 var motion = Vector2(0, 0)
@@ -18,6 +18,16 @@ func play_walk_left():
 	else:
 		$Sprite_right.visible = false
 		$Sprite_left.visible = true
+		
+func play_walk_right():
+	motion.x = SPEED
+	if is_on_floor():
+		$Sprite_right.play("Walk")
+		$Sprite_right.visible = true
+		$Sprite_left.visible = false
+	else:
+		$Sprite_right.visible = true
+		$Sprite_left.visible = false
 
 
 func _process(delta):
@@ -26,14 +36,15 @@ func _process(delta):
 
 	if Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_left"):
 		
-		motion.x = SPEED
-		if is_on_floor():
-			$Sprite_right.play("Walk")
-			$Sprite_left.visible = false
+		if Input.is_action_just_pressed("ui_click_left"):
+			$Sprite_right.play("1Atack")
 			$Sprite_right.visible = true
+			$Sprite_left.visible = false
 		else:
-			$Sprite_left.visible = false
-			$Sprite_right.visible = true
+			if $Sprite_right.get_animation() != "1Atack":
+				play_walk_right()
+			elif $Sprite_right.frame == $Sprite_right.frames.get_frame_count("1Atack") - 1:
+				play_walk_right()
 	elif Input.is_action_pressed("ui_left") && !Input.is_action_pressed("ui_right"):
 		if Input.is_action_just_pressed("ui_click_left"):
 			$Sprite_left.play("1Atack")
@@ -49,20 +60,22 @@ func _process(delta):
 		if $Sprite_left.visible:
 			$Sprite_left.play("1Atack")
 		elif $Sprite_right.visible:
-			$Sprite_right.play("Idle")
+			$Sprite_right.play("1Atack")
 	else:
 		if $Sprite_left.visible && is_on_floor() && $Sprite_left.get_animation() != "1Atack":
 			$Sprite_left.play("Idle")
-			pass
 		elif $Sprite_right.visible && is_on_floor() && $Sprite_right.get_animation() != "1Atack":
 			$Sprite_right.play("Idle")
 
 	if $Sprite_left.get_animation() == "1Atack" && $Sprite_left.frame == $Sprite_left.frames.get_frame_count("1Atack") - 1:
 			$Sprite_left.play("Idle")
+	
+	if $Sprite_right.get_animation() == "1Atack" && $Sprite_right.frame == $Sprite_right.frames.get_frame_count("1Atack") - 1:
+			$Sprite_right.play("Idle")
 			
 
 	if is_on_floor():
-		if Input.is_action_pressed("ui_up"):
+		if Input.is_action_pressed("ui_up") && motion.y > 0:
 			if $Sprite_right.visible:
 				$Sprite_right.play("Jump")
 			else:
@@ -72,7 +85,9 @@ func _process(delta):
 			motion.y = JUMP
 
 	if $Sprite_left.get_animation() == "1Atack" &&  Input.is_action_pressed("ui_left"):
-		motion.x = -ATACK_SPEED
+		motion.x = -ATACK_SPEED	
+	if $Sprite_right.get_animation() == "1Atack" &&  Input.is_action_pressed("ui_right"):
+		motion.x = ATACK_SPEED
 
 	motion = move_and_slide(motion, UP)
 	motion.x = 0

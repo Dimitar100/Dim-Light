f8b8dd45856ba97e421303334b1dec39bd567e95
BuildTools <stoilovdimitar29@gmail.com>
2021-09-14 22:21:25 +0300

    friendly NPC added

---
+++ NPC.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+const SPEED = 250
+const GRAVITY = 2000
+const JUMP = -350
+const WIND_TIMER = 3
+
+var motion = Vector2(0, 0)
+var target = Vector2(0, 0)
+
+func _ready():
+	var root_node = get_parent()
+	var child_two = root_node.get_node("End")
+	target = child_two.global_position
+
+func _process(delta):
+	
+	motion.y += GRAVITY*delta
+	
+	if  global_position.x > target.x:
+		motion.x = -SPEED
+	elif global_position.x < target.x:
+		motion.x = SPEED
+				
+	motion = move_and_slide(motion, UP)
+	motion.x = 0

5e27191ced08cf07c5ff6dbcea4f5eb205284b97
BuildTools <stoilovdimitar29@gmail.com>
2021-10-04 22:52:20 +0300

    small progress on the state machine

--- StateMachinePlayer.gd
+++ StateMachinePlayer.gd
@@ -1,25 +1,32 @@
 extends StateMachine
 
 func _ready():
-	add_state("Idle")
-	add_state("Walking")
-	call_deferred("set_state", states.Idle)
+	add_state("idle")
+	add_state("walk")
+	add_state("fall")
+	add_state("jump")
+	add_state("atack")
+	call_deferred("set_state", states.idle)
 
-#func _input(event):
+func _input(_event):
+	if [states.idle, states.walk].has(state):
+		#JUMP
+		pass
 
 func _state_logic(delta):
+	parent._handle_move_input()
 	parent._apply_gravity(delta)
 	parent._apply_movement(delta)
 
 func _get_transition(_delta):
 	match state:
-		states.Idle:
+		states.idle:
 			pass
 
 	
 	return null
 				
-func _enter_state(new_state, _old_state):
+func _enter_state(_new_state, _old_state):
 	pass
 	
 func _exit_state(_old_state, _new_state):

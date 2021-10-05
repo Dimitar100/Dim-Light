9201e71cdff6c3c8488415ef2acc07d3c1ac5187
BuildTools <stoilovdimitar29@gmail.com>
2021-10-05 22:37:59 +0300

    almost done with the player state machine

--- StateMachinePlayer.gd
+++ StateMachinePlayer.gd
@@ -6,12 +6,21 @@ func _ready():
 	add_state("fall")
 	add_state("jump")
 	add_state("atack")
+	add_state("atack_move")
 	call_deferred("set_state", states.idle)
 
 func _input(_event):
-	if [states.idle, states.walk].has(state):
+	if [states.idle, states.walk, states.atack].has(state):
 		#JUMP
-		pass
+		if Input.is_action_pressed("ui_up"):
+			parent.motion.y = parent.JUMP
+	if state != states.jump:
+		if Input.is_action_just_pressed("ui_click_left") && (Input.is_action_pressed("ui_right") || Input.is_action_pressed("ui_left")):
+			parent.atack = true
+			parent.motion.x = parent.ATACK_SPEED * parent.move_direction
+		elif Input.is_action_just_pressed("ui_click_left"):
+			parent.atack = true
+			#pass
 
 func _state_logic(delta):
 	parent._handle_move_input()
@@ -21,13 +30,66 @@ func _state_logic(delta):
 func _get_transition(_delta):
 	match state:
 		states.idle:
-			pass
+			if !parent.is_on_floor():
+				if parent.motion.y < 0:
+					return states.jump
+				if parent.motion.y >= 0:
+					return states.fall
+			elif parent.motion.x != 0:
+				return states.walk
+			elif parent.atack:
+				return states.atack
+		states.walk:
+			if !parent.is_on_floor():
+				if parent.motion.y < 0:
+					return states.jump
+				if parent.motion.y >= 0:
+					return states.fall
+			elif parent.motion.x == 0:
+				return states.idle
+			elif parent.atack:
+				return states.atack
+		states.jump:
+			if parent.is_on_floor():
+				return states.idle
+			elif parent.atack:
+				return states.atack
+			elif parent.motion.y >= 0:
+				return states.fall
+		states.fall:
+			if parent.is_on_floor():
+				print("ok")
+				return states.idle
+			elif parent.motion.y < 0:
+				return states.jump
+			elif parent.atack:
+				print("ok1")
+				return states.atack
+		states.atack:
+			if !parent.is_on_floor():
+				if parent.motion.y < 0:
+					parent.atack = false
+					return states.jump
+				if parent.motion.y > 0:
+					parent.atack = false
+					return states.fall
+			if parent.anim_player.get_animation() == "Atack" && parent.anim_player.frame == parent.anim_player.frames.get_frame_count("Atack") - 1:
+				parent.atack = false
+				return states.idle
 
-	
 	return null
 				
-func _enter_state(_new_state, _old_state):
-	pass
+func _enter_state(new_state, _old_state):
+	match new_state:
+		states.idle:
+			parent.anim_player.play("Idle")
+		states.walk:
+			parent.anim_player.play("Walk")
+		states.jump:
+			parent.anim_player.play("Jump")
+		states.atack:
+			parent.anim_player.play("Atack")
+			
 	
 func _exit_state(_old_state, _new_state):
 	pass
--- player.gd
+++ player.gd
@@ -5,97 +5,29 @@ const SPEED = 650
 const ATACK_SPEED = 200
 const GRAVITY = 2000
 const JUMP = -850
-const WIND_TIMER = 3
 
 var motion = Vector2(0, 0)
 var move_direction
-
-func play_walk_left():
-	motion.x = -SPEED
-	if is_on_floor():
-		$Sprite_left.play("Walk")
-		$Sprite_right.visible = false
-		$Sprite_left.visible = true
-	else:
-		$Sprite_right.visible = false
-		$Sprite_left.visible = true
-		
-func play_walk_right():
-	motion.x = SPEED
-	if is_on_floor():
-		$Sprite_right.play("Walk")
-		$Sprite_right.visible = true
-		$Sprite_left.visible = false
-	else:
-		$Sprite_right.visible = true
-		$Sprite_left.visible = false
-
+var atack = false
+onready var anim_player = get_node("Sprite_right")
 
 func _apply_gravity(delta):
 	motion.y += GRAVITY*delta
 
 func _handle_move_input():
 	move_direction = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	motion.x = SPEED * move_direction 
+	if move_direction > 0:
+		$Sprite_right.visible = true
+		$Sprite_left.visible = false
+		anim_player = $Sprite_right
+	elif move_direction < 0:
+		$Sprite_right.visible = false
+		$Sprite_left.visible = true
+		anim_player = $Sprite_left
 
 func _apply_movement(_delta):
-	
-	if Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_left"):
-		
-		if Input.is_action_just_pressed("ui_click_left"):
-			$Sprite_right.play("1Atack")
-			$Sprite_right.visible = true
-			$Sprite_left.visible = false
-		else:
-			if $Sprite_right.get_animation() != "1Atack":
-				play_walk_right()
-			elif $Sprite_right.frame == $Sprite_right.frames.get_frame_count("1Atack") - 1:
-				play_walk_right()
-	elif Input.is_action_pressed("ui_left") && !Input.is_action_pressed("ui_right"):
-		if Input.is_action_just_pressed("ui_click_left"):
-			$Sprite_left.play("1Atack")
-			$Sprite_right.visible = false
-			$Sprite_left.visible = true
-		else:
-			if $Sprite_left.get_animation() != "1Atack":
-				play_walk_left()
-			elif $Sprite_left.frame == $Sprite_left.frames.get_frame_count("1Atack") - 1:
-				play_walk_left()
-				
-	elif Input.is_action_just_pressed("ui_click_left"):
-		if $Sprite_left.visible:
-			$Sprite_left.play("1Atack")
-		elif $Sprite_right.visible:
-			$Sprite_right.play("1Atack")
-	else:
-		if $Sprite_left.visible && is_on_floor() && $Sprite_left.get_animation() != "1Atack":
-			$Sprite_left.play("Idle")
-		elif $Sprite_right.visible && is_on_floor() && $Sprite_right.get_animation() != "1Atack":
-			$Sprite_right.play("Idle")
-
-	if $Sprite_left.get_animation() == "1Atack" && $Sprite_left.frame == $Sprite_left.frames.get_frame_count("1Atack") - 1:
-			$Sprite_left.play("Idle")
-	
-	if $Sprite_right.get_animation() == "1Atack" && $Sprite_right.frame == $Sprite_right.frames.get_frame_count("1Atack") - 1:
-			$Sprite_right.play("Idle")
-			
-
-	if is_on_floor():
-		if Input.is_action_pressed("ui_up") && motion.y > 0:
-			if $Sprite_right.visible:
-				$Sprite_right.play("Jump")
-			else:
-				$Sprite_left.play("Jump")
-
-			motion.y = JUMP
-
-	if $Sprite_left.get_animation() == "1Atack" &&  Input.is_action_pressed("ui_left"):
-		motion.x = -ATACK_SPEED	
-	if $Sprite_right.get_animation() == "1Atack" &&  Input.is_action_pressed("ui_right"):
-		motion.x = ATACK_SPEED
-		
-	_handle_move_input()
-
 	motion = move_and_slide(motion, UP)
-	motion.x = 0
+	
 
 

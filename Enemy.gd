d34272579b1b0946bffb1ea51c44dc478ed19680
BuildTools <stoilovdimitar29@gmail.com>
2022-03-20 19:02:02 +0200

    working on the new monster mechanics

--- Enemy.gd
+++ Enemy.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const SPEED = 600
-const FAST_SPEED = 700
+export var FAST_SPEED = 600
 const GRAVITY = 2000
 const JUMP = -800
 
@@ -12,23 +12,27 @@ var fast = -1
 var root_node
 var child_two
 var ready = true
-var direction = 1
+var direction = -1
 var on = true
 var start = false
+
+export var point1 = 4916
+export var point2 = 6300
 
 export var tutorial = false
 
 func _ready():
-	if !tutorial:
-		$EyeGlow.pause_mode = true
-		child_two = get_parent().get_parent().get_node("Mage")
-		if child_two == null:
-			queue_free()
-		else:
-			target = child_two.global_position
-			if  global_position.x > target.x:
-				$AnimatedSprite.flip_h = true
-				direction = -1
+	pass
+	#if !tutorial:
+	#	$EyeGlow.pause_mode = true
+	#	child_two = get_parent().get_parent().get_node("Mage")
+	#	if child_two == null:
+	#		queue_free()
+	#	else:
+	#		target = child_two.global_position
+	#		if  global_position.x > target.x:
+	#			$AnimatedSprite.flip_h = true
+	#			direction = -1
 
 func _apply_gravity(delta):
 	if ready && !tutorial:
@@ -57,9 +61,28 @@ func _apply_movement(_delta):
 		#		motion.x = SPEED
 		#else:
 		#	motion.x = 0
-
-		#motion = move_and_slide(motion, UP)
-		$AnimatedSprite.play("Idle")
+		
+		#$CollisionShape2D.disabled = false
+		
+		if  global_position.x >= point1 && direction == -1:
+			$AnimatedSprite.flip_h = true
+		elif global_position.x <= point2 && direction == 1:
+			$AnimatedSprite.flip_h = false
+		else:
+			motion.x = 0
+			
+		if fast > 0:
+			motion.x = direction * FAST_SPEED
+		else:	
+			motion.x = direction * SPEED
+			
+		if global_position.x <= point1:
+			direction = 1
+		elif global_position.x >= point2:
+			direction = -1
+			
+		motion = move_and_slide(motion, UP)
+		
 	else:
 		$AnimatedSprite.play("Idle")
 		

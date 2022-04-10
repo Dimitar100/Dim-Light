52570f4c984cde0729ddfb3bb26993b763aba3d4
BuildTools <stoilovdimitar29@gmail.com>
2022-04-10 11:48:37 +0300

    added movement and walk animation for the undead warrior but there are some issues with the sprites for it

--- UndeadWarrior.gd
+++ UndeadWarrior.gd
@@ -1,17 +1,17 @@
 extends KinematicBody2D
 
 const UP = Vector2(0, -1)
-const SPEED = 600
+const SPEED = 200
 export var FAST_SPEED = 600
 const GRAVITY = 2000
 
 var motion = Vector2(0, 0)
 var target = Vector2(0, 0)
-
+var direction = -1
 var in_range = false
 
-
-
+export var point1 = 4916
+export var point2 = 6300
 export var tutorial = false
 
 func _ready():
@@ -23,12 +23,37 @@ func _apply_gravity(delta):
 
 func _apply_movement(_delta):
 	
+	if !tutorial:
+		
+		if  global_position.x >= point1 && direction == -1:
+			$AnimatedSprite.flip_h = false
+		elif global_position.x <= point2 && direction == 1:
+			$AnimatedSprite.flip_h = true
+		else:
+			motion.x = 0
+			
+		motion.x = direction * SPEED
+			
+		if global_position.x <= point1:
+			direction = 1
+		elif global_position.x >= point2:
+			direction = -1
+			
+		motion = move_and_slide(motion, UP)
+		
+	else:
+		$AnimatedSprite.play("Idle")
+	
 	if in_range:
 		$AnimatedSprite.play("Attack")
 	else:
-		$AnimatedSprite.play("Idle")
+		$AnimatedSprite.play("Walk")
 		
 
 	
 func _on_Range_body_entered(_body):
 	in_range = true
+
+
+func _on_Range_body_exited(_body):
+	in_range = false

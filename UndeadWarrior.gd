580d9554e653b35ed3d2ca73954f169830ee3a7f
BuildTools <stoilovdimitar29@gmail.com>
2022-04-13 20:21:06 +0300

    almost done with the AI for the Undead Warrior but is hard coded

--- UndeadWarrior.gd
+++ UndeadWarrior.gd
@@ -5,7 +5,7 @@ export var FAST_SPEED = 600
 const GRAVITY = 2000
 
 var motion = Vector2(0, 0)
-var target = Vector2(0, 0)
+var target = null
 var direction = -1
 var in_range = false
 var attack = false
@@ -27,23 +27,26 @@ func _apply_gravity(delta):
 func _apply_movement(_delta):
 	
 	if !tutorial:
+		if target != null:
+			point1 = target.global_position.x
+			point2 = target.global_position.x
 		
-		if  global_position.x >= point1 && direction == -1:
+		if  global_position.x >= point1 && direction == -1 && (!$AnimatedSprite.animation == "AttackV2" || $AnimatedSprite.frame > 5):
 			$AnimatedSprite.flip_h = false
-		elif global_position.x <= point2 && direction == 1:
+		elif global_position.x <= point2 && direction == 1 && (!$AnimatedSprite.animation == "AttackV2" || $AnimatedSprite.frame > 5):
 			$AnimatedSprite.flip_h = true
 		else:
 			motion.x = 0
 			
 		motion.x = direction * SPEED
 			
-		if global_position.x <= point1:
+		if global_position.x <= point1  && (!$AnimatedSprite.animation == "AttackV2" || $AnimatedSprite.frame > 5):
 			direction = 1
-		elif global_position.x >= point2:
+		elif global_position.x >= point2  && (!$AnimatedSprite.animation == "AttackV2" || $AnimatedSprite.frame > 5):
 			direction = -1
 			
 		motion = move_and_slide(motion, UP)
-		
+
 	else:
 		pass
 		#$AnimatedSprite.play("Idle")
@@ -69,8 +72,9 @@ func _apply_movement(_delta):
 		
 
 	
-func _on_Range_body_entered(_body):
+func _on_Range_body_entered(body):
 	in_range = true
+	target = body
 
 
 func _on_Range_body_exited(_body):
--- Undead_Warrior_Attack_right.gd
+++ Undead_Warrior_Attack_right.gd
@@ -8,33 +8,29 @@ var collision_loc_x
 var in_motion = false
 var start_loc
 var finished = false
+var started = false
 
 func _ready():
 	root_node = get_parent()
 
 func _physics_process(_delta):
-	if !get_parent().get_node("AnimatedSprite").flip_h:
+	if get_parent().get_node("AnimatedSprite").flip_h:
+		
 		
 		if get_parent().attack && !in_motion && !finished:
 			start_loc = $CollisionShape2D.global_position.x
-			collision_loc_x = start_loc - extend
+			collision_loc_x = start_loc + extend
 			in_motion = true
 			
 		if in_motion:
 			$CollisionShape2D.disabled = false
-			if $CollisionShape2D.global_position.x >= collision_loc_x:
-				$CollisionShape2D.global_position.x = $CollisionShape2D.global_position.x - 1
+			if $CollisionShape2D.global_position.x <= collision_loc_x:
+				$CollisionShape2D.global_position.x = $CollisionShape2D.global_position.x + 1
 			else:
 				$CollisionShape2D.disabled = true
-				$CollisionShape2D.global_position.x = get_parent().get_node("AnimatedSprite").global_position.x - 52 #the space between the animated sprite and the collision shape
+				$CollisionShape2D.global_position.x = get_parent().get_node("AnimatedSprite").global_position.x + 52 #the space between the animated sprite and the collision shape
 				in_motion = false
 				finished = true
 
-
-
 func _on_AttackCooldown_timeout():
 	finished = false
-
-
-func _on_Attack_left_body_entered(_body):
-	print("OK")
